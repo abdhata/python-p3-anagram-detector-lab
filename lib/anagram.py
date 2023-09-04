5a283c359306468aebef813c4dd274cec4642667
abdhata <abdhata@outlook.com>
2023-09-04 20:02:23 +0300

    Completed

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,15 @@
 # your code goes here!
+words = ['enlists', 'google', 'inlets', 'banana']
+
+
+class Anagram:
+    def __init__(self, word=""):
+        self.word = word
+
+    def match(self, words):
+        sorted_word = sorted(self.word)
+        return [w for w in words if sorted(w) == sorted_word]
+
+
+sample = Anagram("listen")
+print(sample.match(words))
